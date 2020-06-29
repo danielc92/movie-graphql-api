fbcbd1398db3f60ca16ff1dfe7413625b35e532b
Daniel Corcoran <danielc.92@outlook.com>
2020-06-29 19:46:17 +1000

    Created GQL types for Actor Entity

---
+++ src/graphql/Actor/index.ts
@@ -0,0 +1,35 @@
+import {
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLInt,
+} from "graphql"
+
+export const ActorType = new GraphQLObjectType({
+  name: "ActorType",
+  description: "A actor type.",
+  fields: () => {
+    return {
+      id: { type: GraphQLInt },
+      actorFirstName: { type: GraphQLString },
+      actorLastName: { type: GraphQLString },
+      actorDob: { type: GraphQLString },
+      createdAt: { type: GraphQLFloat },
+      updatedAt: { type: GraphQLFloat },
+    }
+  },
+})
+
+export const ActorInputType = new GraphQLInputObjectType({
+  name: "ActorInputType",
+  description: "An actor input type.",
+  fields: () => {
+    return {
+      actorFirstName: { type: new GraphQLNonNull(GraphQLString) },
+      actorLastName: { type: new GraphQLNonNull(GraphQLString) },
+      actorDob: { type: GraphQLString },
+    }
+  },
+})
