8f530f44fbe34905433e4af19c9d15ba70bca83b
Daniel Corcoran <danielc.92@outlook.com>
2020-06-29 21:09:10 +1000

    Created User GQL types

---
+++ src/graphql/User/index.ts
@@ -0,0 +1,35 @@
+import {
+  GraphQLObjectType,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+} from "graphql"
+
+export const UserType = new GraphQLObjectType({
+  name: "User",
+  description: "A user",
+  fields: () => {
+    return {
+      id: { type: GraphQLInt },
+      email: { type: GraphQLString },
+      password: { type: GraphQLString },
+      firstName: { type: GraphQLString },
+      lastName: { type: GraphQLString },
+      createdAt: { type: GraphQLFloat },
+      updatedAt: { type: GraphQLFloat },
+    }
+  },
+})
+
+export const UserInputType = new GraphQLInputObjectType({
+  name: "UserInput",
+  description: "Used to create new users",
+  fields: () => {
+    return {
+      email: { type: new GraphQLNonNull(GraphQLString) },
+      password: { type: new GraphQLNonNull(GraphQLString) },
+    }
+  },
+})
