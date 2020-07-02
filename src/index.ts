00b53a3e6772e7f635f7315a0f8dcd179daa916c
Daniel Corcoran <danielc.92@outlook.com>
2020-07-02 19:42:06 +1000

    Installed and applied helmet

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,14 @@
 import "reflect-metadata"
 import { createConnection } from "typeorm"
 import * as express from "express"
+import * as helmet from "helmet"
 import * as graphqlHTTP from "express-graphql"
 import schema from "./graphql"
 
 createConnection()
   .then(async (connection) => {
-    const middleware = (
-      request: express.Request,
-      response: express.Response,
-      next: express.NextFunction
-    ) => {
-      try {
-        console.log(request.query)
-        next()
-      } catch (error) {
-        return response.status(400)
-      }
-    }
-
     const app = express()
-    app.use(middleware)
+    app.use(helmet())
     app.use(
       "/graphql",
       graphqlHTTP({
