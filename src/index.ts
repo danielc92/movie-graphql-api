8ff8019beac2a416cac20ff119f6adac2807c504
Daniel Corcoran <danielc.92@outlook.com>
2020-07-19 10:19:30 +1000

    Applied cors

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import express from "express"
 import helmet from "helmet"
 import graphqlHTTP from "express-graphql"
 import schema from "./graphql"
+import cors from "cors"
 
 createConnection()
   .then(async (connection) => {
     const app = express()
+    app.use(cors())
     app.use(helmet())
     app.use(
       "/graphql",
